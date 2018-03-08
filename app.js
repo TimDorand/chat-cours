73ddaeab0e5332e2be74c580c98f01fa22a738cc
Arthur Leroux <arthur_leroux@hotmail.fr>
2018-03-08 12:15:27 +0100

    afficahge messages

--- app.js
+++ app.js
@@ -8,51 +8,54 @@ const redis = require("redis");
 const client = redis.createClient();
 
 function consoleLog(event, method, msg = undefined) {
-  console.log(event.red + '.' + method.yellow + (msg !== undefined ? (' => ' + msg) : ''));
+    console.log(event.red + '.' + method.yellow + (msg !== undefined ? (' => ' + msg) : ''));
 }
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
 io.on('connection', function(socket){
-  consoleLog('socket', 'connection', 'another user connected');
+    consoleLog('socket', 'connection', 'another user connected');
 
-  socket.broadcast.emit('hi');
+    socket.broadcast.emit('hi');
 
-  socket.on('chat.join', (data) => {
-    const user_ip = requestIp.getClientIp(socket.request);
-    const json = JSON.parse(data);
+    socket.on('chat.join', (data) => {
+        const user_ip = requestIp.getClientIp(socket.request);
+        const json = JSON.parse(data);
 
-    //1. save username
-    socket.username = json.username;
-    socket.userIp = user_ip;
+        //1. save username
+        socket.username = json.username;
+        socket.userIp = user_ip;
 
-    consoleLog('chat', 'join', `${socket.username} has IP ${user_ip}`);
+        consoleLog('chat', 'join', `${socket.username} has IP ${user_ip}`);
 
-    client.sadd(['users', JSON.stringify({'username': json.username, 'ip': socket.userIp})], (err, res) => {
-      consoleLog('redis', 'SADD', `Add ${socket.username} to user Set`);
-      console.log(res);
+        client.sadd(['users', JSON.stringify({'username': json.username, 'ip': socket.userIp})], (err, res) => {
+            consoleLog('redis', 'SADD', `Add ${socket.username} to user Set`);
+            console.log(res);
+        });
+
+        //2. broadcast
+        socket.broadcast.emit('chat.join', JSON.stringify({'username': socket.username}));
+        client.smembers('users', function(err, res) {
+            if (err) throw(err);
+            console.log(res);
+
+            for (let data of res) {
+                socket.emit('chat.join', data);
+            }
+        });
     });
 
-    //2. broadcast
-    socket.broadcast.emit('chat.join', JSON.stringify({'username': socket.username}));
-    client.smembers('users', function(err, res) {
-      if (err) throw(err);
-      console.log(res);
+    socket.on('chat.message', function(message){
+        consoleLog('chat', 'message', ('[' + socket.username + ']').bold + ' message : ' + message);
+        const json = JSON.stringify({username: socket.username, message});
 
-      for (let data of res) {
-        socket.emit('chat.join', data);
-      }
+        socket.broadcast.emit('chat.message', json);
+        socket.emit('chat.message', json);
     });
-  });
 
-  socket.on('chat.message', function(msg){
-    consoleLog('chat', 'message', ('[' + socket.username + ']').bold + 'message : ' + message);
-    socket.broadcast.emit('chat.message', msg);
-  });
-
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
 });
 
 http.listen(3000, () => console.log('Listening on '  + 'http://localhost:3000\n'.green));
